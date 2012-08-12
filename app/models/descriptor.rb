469c04b1237db97ccc09690cc70803ee7f9ce2cc
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-08-12 16:26:36 +0800

    edit views

--- app/models/descriptor.rb
+++ app/models/descriptor.rb
@@ -1,5 +1,6 @@
 class Descriptor < ActiveRecord::Base
   attr_accessible :datatype, :item_id, :name
   belongs_to :item
+  has_many :values
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   attr_accessible :name
+  has_many :descriptors
 end
+
