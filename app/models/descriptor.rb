ad1bfec0b5e87bf60bd5463f4406d69ef2154b00
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-08-12 21:04:36 +0800

    characteristic typo!

--- app/models/descriptor.rb
+++ app/models/descriptor.rb
@@ -2,6 +2,6 @@ class Descriptor < ActiveRecord::Base
   attr_accessible :datatype, :item_id, :name
   belongs_to :item
   has_many :values
-  has_many :chracteristics
+  has_many :characteristics
 end
 
