b337e49e5ea41f8b83035bf8025b2d49538f5661
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-08-14 12:26:36 +0800

    instance has many instances

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -4,5 +4,11 @@ class Instance < ActiveRecord::Base
   attr_accessible :name, :item_id
   belongs_to :item
   has_many :characteristics
+
+  has_many :subitems, :foreign_key => :parent_id, :dependent => :destroy
+  has_many :parents, :through => :subitems, :source => :parent, :dependent => :destroy
+  has_many :children, :through => :reverse_subitems, :source => :child, :dependent => :destroy
+  has_many :reverse_subitems, :foreign_key => :child_id, :class_name => "Subitem", :dependent => :destroy
+
 end
 
